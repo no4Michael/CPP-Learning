36bc4541490320cd2b637bf44a29b3ab4af2925f
no4Michael <169057679+no4Michael@users.noreply.github.com>
2024-05-06 18:09:03 +0500

    Cpp_Calculator_Test
    Calculator.

---
+++ C++_Calculator1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1;
+    cout << "Enter num1: ";
+    cin >> num1;
+
+    int num2;
+    cout << "Enter num2: ";
+    cin >> num2;
+
+    char sym;
+    cout << "Enter + ; - ; * ; / : ";
+    cin >> sym;
+
+    if (sym == '+') {
+        cout << "Succesfuly: " << num1 + num2 << endl;
+    }
+    else if (sym == '-') {
+        cout << "Succesfuly: " << num1 - num2 << endl;
+    }
+    else if (sym == '*') {
+        cout << "Succesfuly: " << num1 * num2 << endl;
+    }
+    else if (sym == '/') {
+        cout << "Succesfuly: " << num1 / num2 << endl;
+    }
+}
